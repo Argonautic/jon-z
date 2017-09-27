5a9871b0229f1e33eab2ad87c8912a04da0f0f28
Jon <jonathan.zhou93@gmail.com>
2017-09-27 17:03:50 -0400

    text change

--- src/components/content/Detailed.js
+++ src/components/content/Detailed.js
@@ -18,7 +18,7 @@ export default class Detailed extends Component {
                     <div id="portrait-info">
                         <h1 id="portrait-name">Jonathan Zhou</h1>
                         <h3>Software Engineer, Web Developer, Musician, and Creator. Does his best to
-                         make things that do good (and do well!)</h3>
+                          make things that do good (and do well!)</h3>
                     </div>
                 </div>
 
