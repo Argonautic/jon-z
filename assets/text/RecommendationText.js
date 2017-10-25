379db10294eed67e1fc691e1ca9ed83050a8f0bb
Jon <jonathan.zhou93@gmail.com>
2017-10-24 22:49:06 -0400

    Minor UI fixes

--- assets/text/RecommendationText.js
+++ assets/text/RecommendationText.js
@@ -1,14 +1,14 @@
-export const dannaRecommendation = 'Jon Zhou is hands down one of my all-time favorite colleagues. Throughout his tenure at ' +
+export const dannaRecommendation = '"Jon Zhou is hands down one of my all-time favorite colleagues. Throughout his tenure at ' +
     'Argyle, he proved to be a fast learner, a self-starter, and a helpful team member. He holds himself to the highest of ' +
     'standards and consistently delivered above and beyond what was asked.\n\nAll of this, combined with his positive attitude ' +
-    'and clever sense of humor, made having him on my team the utmost pleasure.';
+    'and clever sense of humor, made having him on my team the utmost pleasure."';
 
-export const dinaRecommendation = 'Jonathan Zhou always figures it out! I worked with Jonathan at Argyle and if there was a ' +
+export const dinaRecommendation = '"Jonathan Zhou always figures it out! I worked with Jonathan at Argyle and if there was a ' +
     'problem he would solve it. Whether it is fixing wonky html, creating the perfect landing page, or creating script, he would ' +
-    'get it done. Jonathan is innovative, detail-oriented and a team player. He would be an asset to any team.';
+    'get it done. Jonathan is innovative, detail-oriented and a team player. He would be an asset to any team."';
 
-export const yukkeeRecommendation = "It is my absolute pleasure to recommend Jonathan Zhou as a front end developer. Jonathan " +
-    "has worked as an intern at WrkBook LLC for 11 weeks. I thoroughly enjoyed my time working with Jonathan, and came to know " +
-    "him as a truly valuable asset to any team.\n\nHe is hard-working, eager to learn, and always motivates the team. Beyond that, " +
-    "he has grown tremendously this past summer as our intern learning the Javascript frameworks React JS and Meteor JS. Along " +
-    "with his undeniable grit, Jonathan has been an absolute joy to work with, he is a true team player.";
+export const yukkeeRecommendation = '"It is my absolute pleasure to recommend Jonathan Zhou as a front end developer. Jonathan ' +
+    'has worked as an intern at WrkBook LLC for 11 weeks. I thoroughly enjoyed my time working with Jonathan, and came to know ' +
+    'him as a truly valuable asset to any team.\n\nHe is hard-working, eager to learn, and always motivates the team. Beyond that, ' +
+    'he has grown tremendously this past summer as our intern learning the Javascript frameworks React JS and Meteor JS. Along ' +
+    'with his undeniable grit, Jonathan has been an absolute joy to work with, he is a true team player."';
